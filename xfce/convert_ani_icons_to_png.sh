0f152c3a1485313f13ec83b3d6675e8c3d11bb30
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-11-04 10:33:27 +0100

    Extend bash script to automatically build x11 icons
    (given config files are provided)

--- xfce/convert_ani_icons_to_png.sh
+++ xfce/convert_ani_icons_to_png.sh
@@ -14,6 +14,30 @@ for CURSOR_FILE_PATH in "$ORIGINAL_CURSOR_IMAGES_ANI_DIR/"*.ani; do
     ./build_cmake/aniFileExtractor "$CURSOR_FILE_PATH" "$CONVERTED_CURSOR_IMAGES_PNG_DIR/$CURSOR_NAME"
     for CURSOR_EXTRACTED_ICO_FILE_PATH in "$CONVERTED_CURSOR_IMAGES_PNG_DIR/$CURSOR_NAME/"*.ico; do
         CUSRSOR_ICO_FILE_NAME=`basename "$CURSOR_EXTRACTED_ICO_FILE_PATH" .ico`
-	    convert "$CURSOR_EXTRACTED_ICO_FILE_PATH" "$CONVERTED_CURSOR_IMAGES_PNG_DIR/$CURSOR_NAME/$CUSRSOR_ICO_FILE_NAME.png"
+        convert "$CURSOR_EXTRACTED_ICO_FILE_PATH" "$CONVERTED_CURSOR_IMAGES_PNG_DIR/$CURSOR_NAME/$CUSRSOR_ICO_FILE_NAME.png"
     done
+    X11_CURSOR_OUTPUT_NAMES_FILE_PATH="$ORIGINAL_CURSOR_IMAGES_ANI_DIR/${CURSOR_NAME}_x11_cursor_names.txt"
+    X11_CURSOR_CONFIG_FILE_PATH="$ORIGINAL_CURSOR_IMAGES_ANI_DIR/${CURSOR_NAME}.cursor"
+    if test -f "$X11_CURSOR_OUTPUT_NAMES_FILE_PATH"; then
+        echo "Found $X11_CURSOR_OUTPUT_NAMES_FILE_PATH -> create X11 icons"
+        X11_CURSOR_OUTPUT_NAMES=($(cat "$X11_CURSOR_OUTPUT_NAMES_FILE_PATH"))
+        OUTPUT_ABSOLUTE_PATH="`realpath "$CONVERTED_CURSOR_IMAGES_PNG_DIR/x11_cursors"`"
+        X11_CURSOR_CONFIG_ABSOLUTE_PATH="`realpath "$X11_CURSOR_CONFIG_FILE_PATH"`"
+        mkdir -p "$CONVERTED_CURSOR_IMAGES_PNG_DIR/x11_cursors"
+        for i in "${X11_CURSOR_OUTPUT_NAMES[@]}"
+        do
+            echo "> Create $i"
+            CWD=$(pwd)
+            cd "$CONVERTED_CURSOR_IMAGES_PNG_DIR/$CURSOR_NAME/"
+            if test -f "$X11_CURSOR_CONFIG_ABSOLUTE_PATH"; then
+                echo ">> Found $X11_CURSOR_CONFIG_FILE_PATH"
+                set -x
+                xcursorgen "$X11_CURSOR_CONFIG_ABSOLUTE_PATH" "$OUTPUT_ABSOLUTE_PATH/$i"
+                set +x
+            else
+                echo ">> $X11_CURSOR_CONFIG_FILE_PATH not found!"
+            fi
+            cd "$CWD"
+        done
+    fi
 done
